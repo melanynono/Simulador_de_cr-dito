2d6383fe5c8a876ba9f52cbea567ffb581302676
melanynono <melanynono123@gmail.com>
2026-07-05 20:10:06 -0500

    Implementar función calcular y mostrar disponible

--- simulador.js
+++ simulador.js
@@ -1,1 +1,10 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+
+function calcular(){
+
+    let ingresos = parseFloat(document.getElementById("txtIngresos").value);
+    let egresos = parseFloat(document.getElementById("txtEgresos").value);
+    let disponible = calcularDisponible(ingresos, egresos);
+    document.getElementById("spnDisponible").textContent = disponible.toFixed(2);
+
+}
