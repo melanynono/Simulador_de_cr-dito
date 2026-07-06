bef0d0f70b00ec7ce774884fe7dd81c4448258b5
melanynono <melanynono123@gmail.com>
2026-07-05 20:02:41 -0500

    Crear función calcularDisponible

--- funciones.js
+++ funciones.js
@@ -1,1 +1,12 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+
+function calcularDisponible(ingresos, egresos){
+
+    let disponible = ingresos - egresos;
+
+    if(disponible < 0){
+        disponible = 0;
+    }
+
+    return disponible;
+}
